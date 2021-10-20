f5d7d163b9af8f8dc067832879f4382d16071769
Raul Sevilla <rsevilla@redhat.com>
2021-10-20 12:52:22 +0200

    Simplify invocation with run.sh
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash -ex
+
+set -ex
+
+export WORKLOAD_TEMPLATE=${PWD}/configmap-scale.yml
+export METRICS_PROFILE=${PWD}/metric-profiles/etcdapi.yml
+export ALERTS_PROFILE=${PWD}/alert-profiles/etcdapi-alerts.yml
+export METADATA_COLLECTION=false
+export JOB_TIMEOUT=1800
+
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking.git --depth=1
+pushd e2e-benchmarking/workloads/kube-burner
+
+# Trigger workload
+./run_custom_workload_fromgit.sh
