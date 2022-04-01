6095f2cc7b7e82f7f6ff1fd6b994c2511fea8bbd
Raul Sevilla <rsevilla@redhat.com>
2022-04-01 12:32:55 +0200

    Use local kube-burner
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- run.sh
+++ run.sh
@@ -2,15 +2,9 @@
 
 set -ex
 
-export WORKLOAD_TEMPLATE=${PWD}/configmap-scale.yml
-export METRICS_PROFILE=${PWD}/metric-profiles/etcdapi.yml
-export ALERTS_PROFILE=${PWD}/alert-profiles/etcdapi-alerts.yml
-export METADATA_COLLECTION=false
-export JOB_TIMEOUT=1800
-export WORKLOAD=custom
+prom_url=https://$(oc get route -n openshift-monitoring prometheus-k8s  -o jsonpath="{.spec.host}")
+token=$(oc sa get-token -n openshift-monitoring prometheus-k8s)
 
-git clone https://github.com/cloud-bulldozer/e2e-benchmarking.git --depth=1
-pushd e2e-benchmarking/workloads/kube-burner
-
+curl -L https://github.com/cloud-bulldozer/kube-burner/releases/download/v0.15.4/kube-burner-0.15.4-Linux-x86_64.tar.gz | tar xz
 # Trigger workload
-./run.sh
+./kube-burner init -c ${PWD}/configmap-scale.yml -m ${PWD}/metric-profiles/etcdapi.yml -a ${PWD}/alert-profiles/etcdapi-alerts.yml -u ${prom_url} -t ${token}
